a2e4d0d58d6b2e1b58f49c2801ff723378741d14
AngelinaGerts <angel.ino4ka@mail.ru>
2017-06-16 13:05:47 +0300

    Update xs128p.py

--- xs128p.py
+++ xs128p.py
@@ -124,15 +124,15 @@ def main():
         # Safari tests have always turned up UNSAT
         # Wait for an update from Apple?
     # browser = 'safari'
-    # browser = 'chrome'
-     browser = 'firefox'
+     browser = 'chrome'
+    # browser = 'firefox'
     print 'BROWSER: %s' % browser
 
     # In your browser's JavaScript console:
     # _ = []; for(var i=0; i<5; ++i) { _.push(Math.random()) } ; console.log(_)
     # Enter at least the 3 first random numbers you observed here:
-    dubs = [0.807238201333032526359759601629, 0.534588817864327927860307221469, 0.021134662213999466230883668637, 0.390099492713366520987531922041]
-    if browser == 'firefox':
+    dubs = [0.478490447271263813390040544024, 0.030126952107559647314610248693, 0.932544399037152619676287031399, 0.087201825363375673500100614222]
+    if browser == 'chrome':
         dubs = dubs[::-1]
 
     print dubs
